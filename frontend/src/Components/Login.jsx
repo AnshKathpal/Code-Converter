47537a4026b93cc4b3b59ad8fba698bb972ca03c
AnshKathpal <storage.2.3107@gmail.com>
2023-09-24 19:32:46 +0530

    auth

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,6 +1,6 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import { Button } from '@chakra-ui/react';
-import { useNavigate } from 'react-router-dom'; // Make sure to import this hook
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button,Text } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom"; // Make sure to import this hook
 
 export const Login = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
@@ -8,17 +8,35 @@ export const Login = () => {
 
   const handleLogin = () => {
     if (isAuthenticated) {
-      navigate('/converter');
+      navigate("/converter");
     } else {
       loginWithRedirect();
     }
   };
 
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        width: "100%",
+        backgroundColor : "#183D3D",
+        gap : "30%",
+        flexDirection : "column"
+      }}
+    >
+        <Text
+        fontFamily={"'Press Start 2P', cursive;"}
+        color="white"
+        fontSize={"5xl"}
+      >
+        Code Converter
+      </Text>
       <Button onClick={handleLogin}>
-        {isAuthenticated ? 'Go to Converter' : 'Login with Github'}
+        {isAuthenticated ? "Go to Converter" : "Login with Github"}
       </Button>
-    </>
+    </div>
   );
 };
